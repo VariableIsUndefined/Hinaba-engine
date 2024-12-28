23376ac3663a6f42a3c35d2aa578362c79ce39b4
Leonie <ronon.obvinitely56@gmail.com>
2024-12-28 17:32:08 +0500

    Fixed Reply-Preview

--- static/js/reply-previews.js
+++ static/js/reply-previews.js
@@ -5,46 +5,59 @@ function show_preview(){
 
     var is_thread = href.split("/").slice(-1)[0].includes("#")
 
-    elmnt = this;
+    var elmnt = this;
 
-    $(this).after('<div class="postPreview"><span style="text-align:center;">[...]</span></div>');
+    $("#quote-preview").remove();
+
+    $("body").append('<div id="quote-preview" class="post reply preview reveal-spoilers"><span style="text-align:center;">[...]</span></div>');
+
+    var $preview = $("#quote-preview");
+
+    $(elmnt).on("mousemove", function(e) {
+        $preview.css({
+            top: e.pageY + 10 + "px",
+            left: e.pageX + 10 + "px"
+        });
+    });
 
     jQuery.ajax({
       url: href,
       method: 'GET',
       success: function(data){
         if (is_thread){
-          preview = $(data).find("#" + id).find(".postMessage");
-          preview.find(".hide-reply, .gsearch").detach();
+          preview = $(data).find("#" + id).find(".post");
+          preview.find(".hide-reply").detach();
+          preview.find(".gsearch").detach();
           var thread = preview.html().replace(/\[\]/g, "");
           preview = markdown_parser(thread).autoLink();
         }
         else
         {
           preview = $(data).find("#" + id);
-          preview.find(".Reply-list, .postInfo .fileThumb").detach();
+          preview.find(".reply").detach();
   
           var postInfo = preview.find(".postInfo");
           postInfo.css("margin", 0);
           preview.find(".postMessage").before(postInfo);
-          preview.find("button, .hide-thread, .gsearch").detach();
-  
+
+          preview.find("button").detach();
+          preview.find(".hide-thread").detach();
+          preview.find(".gsearch").detach();
+
           var thread = preview.html().replace(/\[\]/g, "");
           preview = thread;
         }
-        $(elmnt).siblings(".postPreview").html(preview);
+        $preview.html(preview).fadeIn(200);
       }
     });
 }
 
 $( document ).ready(function(){
 
-  // Подключаем событие наведения мыши на ссылки с классом .reference
   $("#container").on("mouseenter", ".reference", show_preview);
 
-  // Удаляем превью при выходе мыши
   $("#container").on("mouseleave", ".reference", function() {
-    $(this).siblings(".postPreview").detach();
+    $("#quote-preview").remove();
   });
 
 });
