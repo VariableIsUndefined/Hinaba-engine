1f1437371a8b86bf7e924f3919f09bd5e841753b
Leonie <ronon.obvinitely56@gmail.com>
2024-12-31 16:33:39 +0500

    Update markdown

--- static/js/post-markdown.js
+++ static/js/post-markdown.js
@@ -1,3 +1,14 @@
+const markdownParser = (text) => {
+  const toHTML = text
+  .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
+  .replace(/\*(.*)\*/gim, '<i>$1</i>') // italic text
+  .replace(/\_\_(.*)\_\_/gim, '<u>$1</u>') // underlined text
+  .replace(/\%\%(.*)\%\%/gim, '<s>$1</s>') // spoiler text
+  .replace(/\`(.*)\`/gim, '<code>$1</code>') // spoiler text
+
+  return toHTML.trim()
+}
+
 function markdown(){
 
   var text_thread = $( ".postMessage" ).toArray();
@@ -6,7 +17,7 @@ function markdown(){
 
     thread_text = $(text_thread[i]).html().replace(/\s+/g, ' ');
 
-    formated_text = markdown_parser(thread_text).autoLink();
+    formated_text = markdownParser(thread_text).autoLink();
 
     $(text_thread[i]).html(formated_text);
 
@@ -22,7 +33,7 @@ function card_text(){
 
     thread_text = $(text_thread[i]).html().replace(/\s+/g, ' ');
 
-    formated_text = markdown_parser(thread_text);
+    formated_text = markdownParser(thread_text);
 
     $(text_thread[i]).html(formated_text);
 
